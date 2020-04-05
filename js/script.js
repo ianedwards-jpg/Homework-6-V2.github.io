1b32b4da82ac5761eea22a41e4c84e0a36d4c4dd
Ian Edwards <ian.edward@uconn.edu>
2020-04-05 11:30:45 -0400

    update

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@
           url: queryURL,
           method: "GET"
         }).then(function(response) {
+          // Iterate over response 5 times for 5-day forecast
+          for (var i = 0; i < 5; i++) {
+
 
           console.log(response);
           // Creating a div to hold the current weather
@@ -65,17 +68,18 @@
 
         //var zipLocation = $("#data-name").val();
         var zipLocation = $("#movie-input").val().trim();
-        //var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=06804,us&appid=cee88101192942cc1ddef8fb37f11635";
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipLocation + ",us&appid=cee88101192942cc1ddef8fb37f11635";
-        // Creating an AJAX call for the current weather 
+        // Creating an AJAX call for the 5 day firecast
         $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-
+  
           console.log(response);
           // Creating a div to hold the current weather
-          var weatherDiv= $("<div class='weatherInfo'>");
+          var colDiv= $("<div class='col'>");
+
+          var weatherCard= $("<div class='weatherInfo'>");
 
           // Storing the weather data
           var weather = response.weather[0].main;
@@ -115,32 +119,32 @@
           weatherDiv.append(image);
 
           // Putting the entire movie above the previous movies
-          $("#weather-view").prepend(weatherDiv);
+          $("#fiveDayView").prepend(weatherDiv);
         });
 
       }
 
       // Function for displaying movie data
-      function renderButtons() {
+      // function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
+      //   // Deleting the movies prior to adding new movies
+      //   // (this is necessary otherwise you will have repeat buttons)
+      //   $("#buttons-view").empty();
 
-        // Looping through the array of movies
-        for (var i = 0; i < movies.length; i++) {
+      //   // Looping through the array of movies
+      //   for (var i = 0; i < movies.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("movie-btn");
-          // Adding a data-attribute
-          a.attr("data-name", movies[i]);
-          // Providing the initial button text
-          a.text(movies[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
+      //     // Then dynamicaly generating buttons for each movie in the array
+      //     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      //     var a = $("<button>");
+      //     // Adding a class of movie-btn to our button
+      //     a.addClass("movie-btn");
+      //     // Adding a data-attribute
+      //     a.attr("data-name", movies[i]);
+      //     // Providing the initial button text
+      //     a.text(movies[i]);
+      //     // Adding the button to the buttons-view div
+      //     $("#buttons-view").append(a);
         }
       }
 
